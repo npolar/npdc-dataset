442f1382b26bdfe42c9ca5979c4f2e70e4875c0d
Anders Bälter <anders.balter@npolar.no>
2015-08-24 17:51:07 +0200

    Fix formular integration and a few minor errors

--- src/edit/DatasetEditController.js
+++ src/edit/DatasetEditController.js
@@ -6,17 +6,14 @@ var DatasetEditController = function ($scope, $controller, $routeParams, Dataset
 
   // EditController -> NpolarUiEditController
   $controller('NpolarEditController', { $scope: $scope });
-  
-  $scope.initFormula = function() {
-    // Inject schema and form(ula)
-    $scope.formula.schema = '//api.npolar.no/schema/dataset';
-    $scope.formula.form = 'edit/formula.json';
-    //$scope.formula.template = 'formula';
+
+  $scope.formula = {
+    schema: '//api.npolar.no/schema/dataset',
+    form: 'edit/formula.json'
   };
 
   // Dataset -> npolarApiResource -> ngResource
   $scope.resource = Dataset;
-  $scope.initFormula();
 
   $scope.expert = function() {
     $scope.formula.form = null;
