e3e5c5eb95648f5d4c38d8071bb645f377e6cfed
cnrdh <cnrdh@users.noreply.github.com>
2015-08-26 08:49:16 +0200

    Using search directive

--- src/search/DatasetSearchController.js
+++ src/search/DatasetSearchController.js
@@ -5,17 +5,13 @@ var angular = require('angular');
  */
 var DatasetSearchController = function ($scope, $location, $controller, Dataset) {
 
-  $controller('NpolarEditController', { $scope: $scope });
+  $controller('NpolarBaseController', { $scope: $scope });
   $scope.resource = Dataset;
-
+    
   $scope.query = function() {
-    var defaults = { limit: 999 };
+    var defaults = { limit: "all", sort: "-updated" };
     var invariants = { fields: 'title,id,updated' };
     return angular.extend(defaults, $location.search(), invariants);
-  };
-
-  $scope.isWriter = function() {
-    return angular.isDefined($scope.user.name);
   };
 
   $scope.search($scope.query());
