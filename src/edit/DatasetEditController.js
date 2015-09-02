ae0dc74d33708c76ac88fc8f0f492ac14759cae7
Anders Bälter <anders.balter@npolar.no>
2015-09-02 13:51:39 +0200

    Hide save and validate buttons

--- src/edit/DatasetEditController.js
+++ src/edit/DatasetEditController.js
@@ -7,13 +7,15 @@ var DatasetEditController = function ($scope, $controller, $routeParams, Dataset
 
   // EditController -> NpolarEditController
   $controller('NpolarEditController', { $scope: $scope });
-  
+
   // Dataset -> npolarApiResource -> ngResource
   $scope.resource = Dataset;
 
   // Formula ($scope.formula set by parent)
   $scope.formula.schema = '//api.npolar.no/schema/dataset';
   $scope.formula.form = 'edit/formula.json';
+  $scope.formula.validateHidden = true;
+  $scope.formula.saveHidden = true;
 
   // edit (or new) action
   $scope.edit();
