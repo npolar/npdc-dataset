3b6d7eaec0b48bff7e5f78fb10d14c395e847c0e
cnrdh <cnrdh@users.noreply.github.com>
2015-09-17 15:12:33 +0200

    Slight renaming

--- src/router.js
+++ src/router.js
@@ -8,14 +8,14 @@ var router = function ($routeProvider, $locationProvider) {
   $locationProvider.html5Mode(true).hashPrefix('!');
 
   $routeProvider.when('/:id', {
-    templateUrl: 'show/show.html',
-    controller: 'DatasetShowController'
+  templateUrl: 'show/show-dataset.html',
+  controller: 'DatasetShowController'
   }).when('/:id/edit', {
-    template: '<npdc:formula></npdc:formula>',
-    controller: 'DatasetEditController'
+  template: '<npdc:formula></npdc:formula>',
+  controller: 'DatasetEditController'
   }).when('/', {
-    template: '<npdc:search></npdc:search>',
-    controller: 'DatasetSearchController'
+  template: '<npdc:search></npdc:search>',
+  controller: 'DatasetSearchController'
   });
 };
 
