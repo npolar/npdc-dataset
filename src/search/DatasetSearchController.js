62ddbd5fc0f24c7c2a4a10bb25bd818ae45d0412
Anders Bälter <anders.balter@npolar.no>
2015-11-03 16:50:03 +0100

    Removed unneccesary code

--- src/search/DatasetSearchController.js
+++ src/search/DatasetSearchController.js
@@ -6,25 +6,13 @@ var DatasetSearchController = function ($scope, $location, $controller, Dataset,
   $controller('NpolarBaseController', { $scope: $scope });
   $scope.resource = Dataset;
   npdcAppConfig.cardTitle = 'Search';
-  $scope.options = {
-    filterUi: {
-      'draft': {
-        type: 'checkbox'
-      },
-      'year-released': {
-        type: 'range'
-      }
-    }
-  };
 
   let defaults = { limit: "50", sort: "-updated,-released", fields: 'title,id,collection,updated', facets: "topics", score: true };
   let invariants = $scope.security.isAuthenticated() ? {} : { "not-draft": "yes", "not-progress": "planned", "filter-links.rel": "data" };
   let query = Object.assign({}, defaults, invariants);
 
   let search = function (q) {
-    $scope.search(Object.assign({}, query, q)).$promise.then((data) => {
-      $scope.options.facets = data.feed.facets;
-    });
+    $scope.search(Object.assign({}, query, q));
   };
 
   search(query);
