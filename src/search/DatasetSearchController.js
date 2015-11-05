0fce44db9c5caa0c665f47e2757e235584ad1f21
Anders Bälter <anders.balter@npolar.no>
2015-11-05 14:02:44 +0100

    Use released as search detail

--- src/search/DatasetSearchController.js
+++ src/search/DatasetSearchController.js
@@ -6,8 +6,11 @@ var DatasetSearchController = function ($scope, $location, $controller, Dataset,
   $controller('NpolarBaseController', { $scope: $scope });
   $scope.resource = Dataset;
   npdcAppConfig.cardTitle = 'Search';
+  npdcAppConfig.search.local.results.detail = function (entry) {
+    return "Released: " + entry.released.split('T')[0];
+  };
 
-  let defaults = { limit: "50", sort: "-updated,-released", fields: 'title,id,collection,updated', facets: "topics", score: true };
+  let defaults = { limit: "50", sort: "-updated,-released", fields: 'title,id,collection,updated,released', facets: "topics", score: true };
   let invariants = $scope.security.isAuthenticated() ? {} : { "not-draft": "yes", "not-progress": "planned", "filter-links.rel": "data" };
   let query = Object.assign({}, defaults, invariants);
 
@@ -20,6 +23,7 @@ var DatasetSearchController = function ($scope, $location, $controller, Dataset,
   $scope.$on('$locationChangeSuccess', (event, data) => {
     search($location.search());
   });
+
 };
 
 module.exports = DatasetSearchController;
