81345f3a1eab50222e74c099a93c7d7031f453e3
Anders Bälter <anders.balter@npolar.no>
2016-02-23 15:59:45 +0100

    i18n and file upload

--- src/edit/DatasetEditController.js
+++ src/edit/DatasetEditController.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var DatasetEditController = function($scope, $controller, $routeParams, Dataset, formula,
-  formulaAutoCompleteService, npdcAppConfig, chronopicService) {
+  formulaAutoCompleteService, npdcAppConfig, chronopicService, fileFunnelService, NpolarLang) {
   'ngInject';
 
   // EditController -> NpolarEditController
@@ -15,6 +15,7 @@ var DatasetEditController = function($scope, $controller, $routeParams, Dataset,
   let formulaOptions = {
     schema: '//api.npolar.no/schema/dataset-1',
     form: 'edit/formula.json',
+    language: NpolarLang.getLang(),
     templates: npdcAppConfig.formula.templates.concat([{
       match(field) {
         return ["alternate", "edit", "via"].includes(field.value.rel);
@@ -35,10 +36,15 @@ var DatasetEditController = function($scope, $controller, $routeParams, Dataset,
     }, {
       match: "placenames_item",
       template: '<npdc:formula-placename></npdc:formula-placename>'
-    }]),
+    }
+  ]),
     languages: npdcAppConfig.formula.languages.concat([{
       map: require('./en.json'),
       code: 'en'
+    },
+    {
+      map: require('./no.json'),
+      code: 'nb_NO',
     }])
   };
 
@@ -52,7 +58,30 @@ var DatasetEditController = function($scope, $controller, $routeParams, Dataset,
   }, format: '{date}'});
 
 
-  $scope.edit();
+  let dataLinkSuccess = function (file) {
+    return {
+      rel: 'data',
+      href: file.url,
+      title: file.filename,
+      length: file.file_size,
+      hash: file.md5sum,
+      type: file.content_type
+    };
+  };
+
+  fileFunnelService.fileUploader({
+    match(field) {
+       return field.id === "links" && field.instance === "data";
+    },
+    server: 'https://apptest.data.npolar.no:3000/dataset/:id/_file/',
+    successCallback: dataLinkSuccess,
+    filterValues: function (value) {
+      return value.rel === 'data';
+    },
+    restricted: false
+  }, $scope.formula);
+
+  $scope.edit(/* generateUUID */ true);
 };
 
 module.exports = DatasetEditController;
