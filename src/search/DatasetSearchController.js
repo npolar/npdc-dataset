4040c4712100a4ebb2f99af6d414cb27e159c609
Anders Bälter <anders.balter@npolar.no>
2016-03-10 09:03:09 +0100

    Add more facets

--- src/search/DatasetSearchController.js
+++ src/search/DatasetSearchController.js
@@ -1,18 +1,30 @@
 'use strict';
 
-var DatasetSearchController = function ($scope, $location, $controller, $filter, Dataset, npdcAppConfig, NpolarTranslate) {
+var DatasetSearchController = function($scope, $location, $controller, $filter, Dataset, npdcAppConfig, NpolarTranslate) {
   'ngInject';
 
-  $controller('NpolarBaseController', { $scope: $scope });
+  $controller('NpolarBaseController', {
+    $scope: $scope
+  });
   $scope.resource = Dataset;
   npdcAppConfig.cardTitle = 'npdc.app.Title';
-  npdcAppConfig.search.local.results.detail = function (entry) {
+  npdcAppConfig.search.local.results.detail = function(entry) {
     return NpolarTranslate.translate("Released: ") + (entry.released ? $filter('date')(entry.released.split('T')[0]) : '-');
   };
 
   let query = function() {
-    let defaults = { limit: "50", sort: "-updated,-released", fields: 'title,id,collection,updated,released', facets: "topics", score: true };
-    let invariants = $scope.security.isAuthenticated() ? {} : { "not-draft": "yes", "not-progress": "planned", "filter-links.rel": "data" };
+    let defaults = {
+      limit: "50",
+      sort: "-updated,-released",
+      fields: 'title,id,collection,updated,released',
+      facets: "topics,tags,people.email,organisation.name",
+      score: true
+    };
+    let invariants = $scope.security.isAuthenticated() ? {} : {
+      "not-draft": "yes",
+      "not-progress": "planned",
+      "filter-links.rel": "data"
+    };
     return Object.assign({}, defaults, invariants);
   };
 
