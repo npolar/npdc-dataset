f9d808b09f7c0924214b9b49ad9ab834b95ac5f8
cnrdh <cnrdh@users.noreply.github.com>
2016-11-14 14:05:38 +0100

    Added warnings, citation, fixed authors

--- src/DatasetModel.js
+++ src/DatasetModel.js
@@ -1,14 +1,21 @@
 'use strict';
 
-function DatasetModel($location, NpolarTranslate) {
+function DatasetModel($location, $q, NpolarTranslate,
+  DatasetCitation, Publication, Project) {
   'ngInject';
+  
+  let self = this;
   
   this.isNyÅlesund = () => {
     return (/\/ny\-[åa]lesund\//).test($location.path());
   };
   
+  this.isDoi = (str) => {
+    return (/^10[.][0-9]+[/].+/).test(str);
+  };
+
   this.getAppTitle = () => {
-    if (this.isNyÅlesund()) {
+    if (self.isNyÅlesund()) {
       return [
         {'@language': 'en', '@value': 'Ny-Ålesund datasets'},
         {'@language': 'no', '@value': 'Ny-Ålesund datasett'}
@@ -22,96 +29,202 @@ function DatasetModel($location, NpolarTranslate) {
     
   };
   
+  this.findRelated = (dataset, resource) => {
+    let relatedDatasets = resource.array({
+        q: dataset.title,
+        fields: 'id,title,collection',
+        score: true,
+        limit: 5,
+        'not-id': dataset.id,
+        op: 'OR'
+      }).$promise;
+      let relatedPublications = Publication.array({
+        q: dataset.title.replace(/\/\"\'\(\)/g, ''),
+        fields: 'id,title,published,collection',
+        score: true,
+        limit: 5,
+        op: 'OR'
+      }).$promise;
+      let relatedProjects = Project.array({
+        q: dataset.title,
+        fields: 'id,title,collection',
+        score: true,
+        limit: 5,
+        op: 'OR'
+      }).$promise;
+      return $q.all([relatedDatasets, relatedPublications, relatedProjects]);
+  };
+  
   this.authors = (dataset) => {
 
-    var folks = [];
-    var orgs = [];
-
-    if (dataset.people instanceof Array) {
-      folks = dataset.people.filter(p => p.roles.includes("author"));
+    let authors = [];
+    
+    if (dataset && dataset.people && dataset.people.length > 0) {
+      authors = dataset.people.filter(p => p.roles.includes("author"));
     }
-
-    if (folks.length === 0 && dataset.organisations instanceof Array) {
-      orgs = dataset.organisations.filter(o => o.roles.includes("author"));
+    if (!authors || authors.length === 0) {
+      if (dataset && dataset.organisations && dataset.organisations.length > 0) {
+        authors = dataset.organisations.filter(o => o.roles.includes("author"));
+      }
     }
-    return folks.concat(orgs);
-
+    return authors;
   };
-
-  this.author_names = (dataset) => {
-    var et_al_suffix = "";
-    var all_authors = this.authors(dataset);
-    if (all_authors.length > 5) {
-      all_authors = [all_authors[0]];
-      et_al_suffix = " et al";
-    }
-    var names = all_authors.map(a => {
-      return a.hasOwnProperty("name") ? a.name : `${a.first_name[0]} ${a.last_name}`;
-    });
-    return names.join(", ") + et_al_suffix;
+  
+  this.relations = (dataset, rels=['parent','publication','project']) => {
+    if (!dataset) { return; }
+    return (dataset.links||[]).filter(l => rels.includes(l.rel));
   };
+  
   
   this.publisher = (dataset) => {
     let p = (dataset.organisations||[]).find(o => o.roles.includes("publisher"));
-
     if (p) {
       return p;
     } else {
-      return {};
+      return { };
     }
   };
   
   this.published_year = (dataset) => {
-    let y = "not-yet released";
-    if ((/^\d{4}\-/).test(dataset.released)) {
+    let y;
+    if (dataset && dataset.released && (/^\d{4}/).test(dataset.released)) {
       y = new Date(dataset.released).getFullYear();
+    } else {
+      y = "not released";
+      //y = new Date(dataset.created).getFullYear();
     }
     return y;
   };
   
-   
- this.uri = (dataset) => {
-  if (!dataset.links) {
-      dataset.links = [];
-  }
-  // Use DOI if set
-  let link = dataset.links.find(l => l.rel === "doi");
-  if (link) {
-    // normalize DOI
-    return link.href;
-  }
-    
-  link = (dataset.links||[]).find(l => {
-      return l.rel === "alternate" && (/html$/).test(l.type);
-    });
-    link = (dataset.links||[]).find(l => {
-      return l.rel === "alternate" && (/html$/).test(l.type);
-    });
-    if (link) {
-      return link.href.replace(/^http:/, "https:");
+ 
+  // URI (web address) of the dataset  
+  this.uri = (dataset) => {
+    // Use DOI if set
+    if (dataset.doi && self.isDoi(dataset.doi)) {
+      let f = dataset.doi.split(/^10./);
+      return `https://doi.org/10.${f[1]}`;
     } else {
-      return `https://data.npolar.no/dataset/${ dataset.id }`;
+      let n = '';
+      if (self.isNyÅlesund(dataset)) {
+        n = 'ny-ålesund/';
+      }
+      return `https://data.npolar.no/dataset/${n}${ dataset.id }`;
     }
   };
 
-  // Citation string
-  this.citation = (dataset) => {
-    let citation = '';
-    
-    if (dataset.citation && dataset.citation.length > 0) {
-      return dataset.citation;
+  // Citation helper
+  this.citation = (dataset, style) => {
+    if (!dataset) {
+      return;
     }
-
-    if ((/^(Norwegian Polar Institute|Norsk Polarinstitutt)$/).test(this.author_names(dataset))) {
-      citation = `${ dataset.title } (${ this.published_year(dataset) }). ${ this.publisher(dataset).name}`;    
-      citation += ` (Tromsø, Norway):`;
+    
+    let authors = self.authors(dataset);
+    let author = authors;
+    let year = self.published_year(dataset);
+    let title = dataset.title;
+    let type;
+    let pub = self.publisher(dataset);
+    let publisher = pub.name || pub.id;
+    let uri = self.uri(dataset);
+    let url = uri;
+    let doi = dataset.doi;
+    
+    if ((/apa/i).test(style)) {
+      type = 'Data set';
+      return DatasetCitation.apa({ authors, year, title, type, publisher, uri });
+    } else if ((/bibtex/i).test(style)){
+      type = '@misc';
+      return DatasetCitation.bibtex({ title, url, doi, type, publisher, author, year, id: dataset.id });      
+    } else if ((/csl/i).test(style)){
+      type = 'dataset';
+      let issued = { 'date-parts': [year] };
+      return DatasetCitation.csl({ type, DOI: doi, URL: url, title, publisher, issued, author });     
     } else {
-      citation = `${ this.author_names(dataset) } (${ this.published_year(dataset) }). `;
-      citation += `${ dataset.title }. ${ this.publisher(dataset).name }`;
+      throw `Uknown citation style: ${style}`;
     }
-    citation += ' '+ this.uri(dataset);
+  };
+  
+  // List of available citations, use href and header for services
+  this.citationList = (dataset) => {
+    return [{ text: self.citation(dataset, 'apa'), title: 'APA'},
+      { text: self.citation(dataset, 'bibtex'), title: 'BibTeX'},
+      { text: self.citation(dataset, 'csl'), title: 'CSL JSON'},
+    ].sort((a,b) => a.title.localeCompare(b.title));
+  };
+  
+  this.warnings = (dataset) => {
+    let w = [];
     
-    return citation;
-  }; 
+    let hasData = (self.relations(dataset, ['data', 'service']).length > 0);
+    let hasAuthors = (self.authors(dataset).length > 0);
+    let hasReleaseYear = (/^[0-9]{4}/).test(dataset.released);
+    let now = new Date();
+    let released;
+     
+    if (hasReleaseYear) {
+      released = Date.parse(dataset.released);
+    }
+    
+    if (hasReleaseYear) {
+      let diff = released-now;
+      if (now < released) {
+        let days_until_release = parseInt(diff/86400000);
+        console.log('Data release in', days_until_release, 'days');
+      }
+    }
+    
+    if (!hasData) {
+      w.push('No data');
+    }
+      
+    if (!hasData && hasReleaseYear) {
+      if (new Date() > released) {
+        w.push('Release date is in the past');
+      }
+    }
+    
+    //if (hasData) {
+      // @todo check links!?
+    //}
+    if (!hasAuthors) {
+      w.push('No authors');
+    }
+    if (!self.publisher(dataset).name) {
+       w.push('No publisher');
+    }
+    if (!hasReleaseYear) {
+      w.push('No release date');
+    }
+    if (hasData && dataset.progress === 'planned') {
+      w.push('Data is published while progress = "planned"');
+    }
+    if (dataset.draft === 'yes' && dataset.progress !==  'planned' ) {
+      w.push('Draft but progress is set to '+dataset.progress);
+    }
+    if (dataset.draft === 'yes' || dataset.progress ===  'planned' ) {
+      w.push('Planned / draft');
+    }
+    if (self.authors(dataset).length === 0) {
+        w.push('No authors (required to assign DOI)');
+    }
+    if (!dataset.iso_topics || dataset.iso_topics.length === 0) {
+        w.push('No ISO topics');
+    }
+    if (!dataset.sets || dataset.sets.length === 0) {
+        w.push('No sets');
+    }
+    if (!dataset.topics || dataset.topics.length === 0) {
+        w.push('No topics');
+    }
+    if (dataset.topics.length === 1 && dataset.topics[0] === 'other') {
+        w.push('No proper topics');
+    }
+    if (!dataset.doi) {
+      if (w.length > 0) {
+        w.push(`No DOI (because of the ${ w.length === 1 ? 'issue' : 'issues' } above)`);
+      }
+    }
+    return w;
+  };
 }
 module.exports = DatasetModel;
